a10aab47bdc4550a9ae10864f02679d5751cec4a
Dowon Jang <rosewoodowon@gmail.com>
2023-09-19 08:39:05 +0900

    연관관계 매핑 기초 - 객체지향 모델링

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,13 +23,15 @@ public class JpaMain {
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
 
-            Long findTeamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
 
             tx.commit();
         } catch (Exception e) {
